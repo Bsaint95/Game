75edb56d7a39d8d4d3f7e1d0770fe98ccd9c3fb3
Bsaint <62014264+Bsaint95@users.noreply.github.com>
2020-04-19 21:32:07 +0100

    Add files via upload

---
+++ Python Task 3 (Guessing game).py
@@ -0,0 +1,106 @@
+import random
+
+
+def easy_level():
+    print('Welcome to the Easy Level\n'
+          'Guess a number between 1 - 10\n'
+          'You have maximum of 6 trials\n'
+          )
+    
+    secret_number = random.randint(1,10)
+    guess_count = 0
+    guess_limit = 6
+
+    while guess_count < guess_limit:
+        while True:
+            try:
+                guess = int(input('Guess number: '))
+                guess_count += 1
+                print('This is your trial number',guess_count)
+            except:
+                print('Whoops! Be sure the number contain only digits(integers)')
+                continue
+            break
+
+        if guess == secret_number:
+            print('You got it right')
+            break
+        elif guess != secret_number:
+            print('That was wrong')
+
+    else:
+        print('Game Over!')
+
+def medium_level():
+    print('Welcome to the Medium Level\n'
+          'Guess a number between 1 - 20\n'
+          'You have maximum of 4 trials\n'
+          )
+    number = random.randint(1,20)
+    count = 0
+    limit = 4
+
+    while count < limit:
+        while True:
+            try:
+                guess = int(input('Guess number: '))
+                count += 1
+                print('This is your trial number',count)
+            except:
+                print('Whoops! Be sure the number contains only digits(integers)')
+                continue
+            break
+
+        if guess == number:
+            print('You got it right!')
+            break
+        elif guess != number:
+            print('That was wrong!')
+
+    else:
+        print('Game Over!')
+              
+def hard_level():
+    print('Welcome to the Hard Level\n'
+          'Guess a number between 1 - 50\n'
+          'You have a maximum of 3 trials\n'
+          )
+    answer = random.randint(1,50)
+    count_number = 0
+    count_limit = 3
+
+    while count_number < count_limit:
+        while True:
+            try:
+                guess = int(input('Guess number: '))
+                count_number += 1
+                print('This is your trail number',count_number)
+            except:
+                print('Whoops! Be sure the number contains only digits(integers)')
+                continue
+            break
+
+        if guess == answer:
+            print('You got it right!')
+            break
+        elif guess != answer:
+            print('That was wrong!')
+    else:
+        print('Game Over!')
+
+def start_game():
+    print('Welcome to Number Guessing Game')
+    action = input('Select game level; Easy, Medium or Hard: ')
+
+    if action.upper() == 'EASY':
+        easy_level()
+                   
+    elif action.upper() == 'MEDIUM':
+        medium_level()
+           
+    elif action.upper() == 'HARD':
+        hard_level()
+        
+
+start_game()
+
